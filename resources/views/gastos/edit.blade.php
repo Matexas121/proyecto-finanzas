a6f9a4b72aadbaf3e20d23bc1dd216f278dcb353
juarezsantti-code <juarezsantti@gmail.com>
2025-11-04 01:34:21 -0300

    editar definitivo

--- resources/views/gastos/edit.blade.php
+++ resources/views/gastos/edit.blade.php
@@ -24,7 +24,6 @@
             @method("put")
             
             <label>Monto</label>
-            {{-- Añadir la clase 'is-invalid' si hay error en el campo 'monto' --}}
             <input name='monto' type='number' step="0.01" 
                    value='{{ old('monto', $gasto->monto) }}'>
             @error('monto') <span style="color: red;">{{ $message }}</span> @enderror
@@ -32,7 +31,6 @@
             <br>
             
             <label>Fecha</label>
-            {{-- CAMPO FALTANTE #1: fecha --}}
             <input name='fecha' type='date' 
                    value='{{ old('fecha', $gasto->fecha) }}'>
             @error('fecha') <span style="color: red;">{{ $message }}</span> @enderror
@@ -47,9 +45,8 @@
             <br>
 
             <label>Forma de Pago</label>
-            {{-- CAMPO FALTANTE #2: formaPago --}}
-            <select name="formaPago">
-                {{-- Usar el helper 'old' y comprobar el valor actual del gasto --}}
+            {{-- AÑADIDO ID PARA JAVASCRIPT --}}
+            <select name="formaPago" id="formaPagoSelect"> 
                 <option value="efectivo" {{ old('formaPago', $gasto->formaPago) == 'efectivo' ? 'selected' : '' }}>Efectivo</option>
                 <option value="tarjeta" {{ old('formaPago', $gasto->formaPago) == 'tarjeta' ? 'selected' : '' }}>Tarjeta</option>
                 <option value="transferencia" {{ old('formaPago', $gasto->formaPago) == 'transferencia' ? 'selected' : '' }}>Transferencia</option>
@@ -59,25 +56,27 @@
             <br>
 
             <label>Categoría (ID)</label>
-            {{-- CAMPO FALTANTE #3: idCategoria --}}
-            {{-- NOTA: Esto debería ser un SELECT, pero para empezar usaremos un INPUT para el ID --}}
             <input name='idCategoria' type='number' 
                    value='{{ old('idCategoria', $gasto->idCategoria) }}'>
             @error('idCategoria') <span style="color: red;">{{ $message }}</span> @enderror
 
             <br>
             
-            {{-- Aquí deberías incluir los campos de transferencia si formaPago es 'transferencia' --}}
-            {{-- Para simplificar, asume que el gasto siempre fue o nunca fue una transferencia y editas los campos si existen. --}}
-            @if ($gasto->transferencia)
+            {{-- CONTENEDOR DE CAMPOS DE TRANSFERENCIA --}}
+            {{-- Nota: Usamos 'old' para persistir datos después de fallos de validación --}}
+            <div id="camposTransferencia" style="margin-top: 15px;">
                 <label>Alias (Transferencia)</label>
                 <input name='alias' type='text' 
-                       value='{{ old('alias', $gasto->transferencia->alias) }}'>
+                       value='{{ old('alias', $gasto->transferencia->alias ?? '') }}'>
+                @error('alias') <span style="color: red;">{{ $message }}</span> @enderror
                 
+                <br>
+
                 <label>Nombre Destinatario</label>
                 <input name='nombreDestinatario' type='text' 
-                       value='{{ old('nombreDestinatario', $gasto->transferencia->nombreDestinatario) }}'>
-            @endif
+                       value='{{ old('nombreDestinatario', $gasto->transferencia->nombreDestinatario ?? '') }}'>
+                @error('nombreDestinatario') <span style="color: red;">{{ $message }}</span> @enderror
+            </div>
 
 
             <br><br>
@@ -85,4 +84,28 @@
         </form>
     </body>
 </html> 
+
+{{-- SECCIÓN JAVASCRIPT --}}
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const selectElement = document.getElementById('formaPagoSelect');
+        const transferenciaDiv = document.getElementById('camposTransferencia');
+
+        // Función para mostrar u ocultar los campos
+        function toggleTransferenciaFields() {
+            if (selectElement.value === 'transferencia') {
+                transferenciaDiv.style.display = 'block'; // Muestra el div
+            } else {
+                transferenciaDiv.style.display = 'none'; // Oculta el div
+            }
+        }
+
+        // 1. Ejecutar la función inmediatamente al cargar la página
+        // Esto es crucial para que los campos estén visibles si el gasto YA es una transferencia.
+        toggleTransferenciaFields();
+
+        // 2. Ejecutar la función cada vez que se cambia el valor del select
+        selectElement.addEventListener('change', toggleTransferenciaFields);
+    });
+</script>
 @endsection
